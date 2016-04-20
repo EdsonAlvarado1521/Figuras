b151d19e56fd1249ba30fc008e362802ab6584cb
Edson Alvarado <edson-0059@hotmail.com>
2016-04-19 21:10:28 -0700

    Ordenar, rellenar y tamaño aleatorio
    genera figuras de tamaño aleatorio, rellena las figuras y ordena las
    figuras

---
+++ Figura.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    abstract class Figura
+    {
+        protected int X,Y;
+        protected Pen pluma;
+        protected SolidBrush brocha;
+        protected Color color_contorno, color_relleno;
+        protected int ancho, largo;
+
+        public Figura(int x,int y)
+        {
+            X=x;
+            Y=y;
+
+            pluma = new Pen(Color.DarkRed,2);
+            brocha = new SolidBrush(Color.Blue);
+            Random rnd = new Random();
+            ancho = rnd.Next(10, 60);
+            largo =ancho;
+        }
+
+        public abstract void Dibuja(Form f);
+        public int CompareTo(object obj)
+        {
+
+            return this.largo.CompareTo(((Figura)obj).largo);
+        }
+
+    }
+
+    class Rectangulo : Figura
+    {
+        public Rectangulo(int x, int y) : base(x, y) 
+        {
+        }
+        public override void Dibuja(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawRectangle(pluma, X, Y, ancho, largo);
+            g.FillRectangle(brocha, this.X, this.Y, ancho, largo);
+        }
+    }
+
+    class Circulo : Figura
+    {
+        public Circulo(int x, int y)
+            : base(x, y)
+        {
+        }
+        public override void Dibuja(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawEllipse(pluma, X, Y, ancho, largo);
+            g.FillEllipse(brocha, this.X, this.Y, ancho, largo);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    
+    public partial class Form1 : Form
+    {
+        private List<Figura> figuras;
+        enum TipoFigura { Rectangulo, Circulo };
+        private TipoFigura figura_actual;
+        public Form1()
+        {
+
+            figuras = new List<Figura>();
+            InitializeComponent();
+            circuloToolStripMenuItem.Checked = true;
+            figura_actual = TipoFigura.Circulo;
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (MouseButtons.Right == e.Button)
+            {
+                contextMenuStrip1.Show(this, e.X, e.Y);
+            }
+            if (MouseButtons.Left == e.Button && circuloToolStripMenuItem.Checked == true)
+            {
+                Circulo c = new Circulo(e.X, e.Y);
+                c.Dibuja(this);
+                figuras.Add(c);
+            }
+            else if (MouseButtons.Left == e.Button && rectanguloToolStripMenuItem.Checked == true)
+            {
+                Rectangulo r = new Rectangulo(e.X, e.Y);
+                r.Dibuja(this);
+                figuras.Add(r);
+            }
+
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Figura f in figuras)
+            {
+                f.Dibuja(this);
+
+            }
+        }
+
+        private void circuloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            circuloToolStripMenuItem.Checked = true;
+            rectanguloToolStripMenuItem.Checked = false;
+            limpiarToolStripMenuItem.Checked = false;
+        }
+
+        private void rectanguloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rectanguloToolStripMenuItem.Checked = true;
+            circuloToolStripMenuItem.Checked = false;
+            limpiarToolStripMenuItem.Checked = false;
+        }
+      
+        private void limpiarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rectanguloToolStripMenuItem.Checked = false;
+            circuloToolStripMenuItem.Checked = false;
+            limpiarToolStripMenuItem.Checked = true;
+        }
+         private void ordenarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            figuras.Sort();
+            figuras.Reverse();
+
+
+        }
+    }
+}
